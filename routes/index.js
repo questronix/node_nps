d10be90cd16241d1164102827c95e49ca8ed4b23
Bandaggao <ban_daggao@questronix.com.ph>
2018-10-15 13:56:45 +0800

    Add other pages

--- routes/index.js
+++ routes/index.js
@@ -13,5 +13,17 @@ module.exports = function(app) {
    });
    app.get('/login',function(req,res){
     res.render('pages/login')
-});
+   });
+   app.get('/success',function(req,res){
+    res.render('pages/success')
+   });
+   app.get('/error',function(req,res){
+    res.render('pages/error')
+   });
+   app.get('/department',function(req,res){
+    res.render('pages/department')
+   });
+   app.get('/ratings',function(req,res){
+    res.render('pages/ratings')
+   });
 };
